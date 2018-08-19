83ef666a0c97566615caa8885836dbf87f8662da
Johanne Andersen <j.andersen864@gmail.com>
2018-08-19 11:58:38 +0200

    Initial commit, also some folder structure and README

---
+++ README.md
@@ -0,0 +1,14 @@
+# Work timer
+
+##### Work in progress!
+
+A command line work timer that can record time spent on a job and calculate earnings of the time spent.
+
+
+### To do list
+
+ * [ ] Timer functionality
+ * [ ] Argument parsing
+ * [ ] Storing time
+ * [ ] Getting time and time periods
+ * [ ] Calculating earnings
---
+++ src/main.py
@@ -0,0 +1,13 @@
+from argparse import ArgumentParser
+
+def parse_args():
+    """
+    default: Show jobs and total time spent on them
+    -job: which job do I want to time (Open up timer, but don't start it)
+    -period: show total time spent on a job in given period (date1 - date2)
+    -earnings: show total earnings for a given period or session
+    """
+
+
+if __name__ == '__main__':
+    parse_args()
