c48d1cd5be03e1f482717a12f3f7a9933ec5d9d7
Johanne Andersen <j.andersen864@gmail.com>
2018-08-25 10:57:57 +0200

    Stop functionality

--- src/stopwatch.py
+++ src/stopwatch.py
@@ -6,41 +6,32 @@ import sys
 def counter(stopwatch):
     t = threading.currentThread()
     while getattr(t, "do_run", True):
-        sys.stdout.write("\r")
-        sys.stdout.write("{:2d} seconds passed.".format(stopwatch.current_time))
-        sys.stdout.flush()
-        time.sleep(1)
-        stopwatch.current_time += 1
-
-
-class Time:
-    seconds = 0
-
-    def to_hours_and_minutes(self):
-        pass
-
-    def to_minutes(self):
-        pass
+        if stopwatch.is_running:
+            sys.stdout.write("\r")
+            sys.stdout.write("{:2d} seconds passed.".format(stopwatch.current_time))
+            sys.stdout.flush()
+            time.sleep(1)
+            stopwatch.current_time += 1
+        else:
+            time.sleep(1)
 
 
 class Stopwatch:
     def __init__(self):
-        self.isRunning = False
+        self.is_running = False
         self.current_time = 0
         self.clock = threading.Thread(target=counter, args=(self,))
 
     def start(self):
-        print("STARTING!")
-        self.clock.start()
-        self.isRunning = True
+        self.is_running = True
+        if not self.clock.is_alive():
+            self.clock.start()
 
     def stop(self):
-        print("STOPPING!")
-        self.isRunning = False
+        self.is_running = False
 
     def save(self):
-        print("SAVING!")
         print (self.current_time)
-        self.isRunning = False
+        self.is_running = False
         self.clock.do_run = False
         return self.current_time
